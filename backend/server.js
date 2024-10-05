06d443c78c7ee3c9f3f69fab5e0c2ae324c88688
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-06 02:29:43 +0500

    server file updated

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 const mongoURL = process.env.MONGO_ATLAS_URL;
+// const port = process.env.PORT;
 
 mongoose
   .connect(mongoURL)
   .then(() => {
     console.log("App connected to database");
+    // app.listen(port, () => {
+    //   console.log("App is listening on PORT", port);
+    // });
+    //! use this when you are making changes locally
   })
   .catch((error) => {
     console.log(error.message);
